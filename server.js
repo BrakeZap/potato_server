c55ce647634b5f1b050b654c1450659504b41ed1
BrakeZap <kjawesome56@gmail.com>
2024-10-28 16:09:25 -0700

    fix data conversion

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ wss.on('connection', function connection(ws) {
   ws.on('error', console.error)
 
   ws.on('message', function message(data) {
-    if (data == 'potato') {
+    console.log(data.toString('utf-8'))
+    if (data.toString('utf-8') === 'potato') {
       wss.clients.forEach((conn) => {
         if (conn != wss && conn.readyState == conn.OPEN)
           conn.send("fetch")
