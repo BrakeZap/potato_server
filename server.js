0e410879ed3d2c078102f10c70c20ec362cef7bd
BrakeZap <kjawesome56@gmail.com>
2024-10-27 17:29:02 -0700

    node server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("whats good gang")
+const { WebSocketServer } = require("ws");
+
+const wss = new WebSocketServer({port: 8080})
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error)
+
+  ws.on('message', function message(data) {
+    ws.send(data)
+  })
+})
